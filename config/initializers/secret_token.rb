d69a4133eb3b56080cf3bf1c84a53ae560a75d13
Lou Dagostino <ladagostino@gmail.com>
2014-01-22 15:44:30 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AsanaExporter::Application.config.secret_key_base = '54e98f3564b84e0b2b46361f6e8d9a37a406977c46d6d9831dca941fc84d5ba848a73ca78fb01174b3ff4e196c752e0dae8b8f4c95576bbcac9bc4d2457c03c6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AsanaExporter::Application.config.session_store :cookie_store, key: '_asana_exporter_session'
